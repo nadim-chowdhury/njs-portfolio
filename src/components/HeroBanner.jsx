0793357a8d8842344674394f841b312a3bf45610
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-05-08 00:28:39 +0600

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,7 +4,7 @@ import Typewriter from "typewriter-effect";
 export default function HeroBanner() {
   return (
     <div className="py-8 sm:mx-3 lg:mx-0 text-slate-700">
-      <h3 className="text-2xl sm:text-3xl">Hello World!</h3>
+      <h3 className="text-3xl sm:text-4xl">Hello World!</h3>
       <h2 className="text-2xl font-bold sm:text-4xl py-4">
         MySelf Nadim Chowdhury
       </h2>

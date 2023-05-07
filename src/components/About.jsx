034302ae4169981f3a20350a664f32126355378a
Nadim <Nadim@chowdhury.com>
2023-05-07 20:33:13 +0600

    redesigned

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="py-2 sm:mx-3 lg:mx-0">
       <h2 className="title">About Me</h2>
-      <p className="text-slate-600 text-justify text-sm sm:text-lg my-3">
+      <p className="text-slate-600 text-justify text-md sm:text-lg my-3">
         I&apos;m a creative and passionate web developer who likes to build and
         develop websites for anyone. Learning, improving skills, and adapting to
         new technologies are my hobbies. I can give my total effort and perform
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,12 +3,12 @@ import Typewriter from "typewriter-effect";
 
 export default function HeroBanner() {
   return (
-    <div className="py-8 text-slate-700 sm:mx-3 lg:mx-0">
-      <h3 className="text-xl sm:text3xl">Hello World!</h3>
-      <h2 className="text-2xl sm:text-4xl py-4">
-        It&apos;s Me Nadim Chowdhury
+    <div className="py-8 sm:mx-3 lg:mx-0 text-slate-700">
+      <h3 className="text-2xl sm:text-3xl">Hello World!</h3>
+      <h2 className="text-2xl font-bold sm:text-4xl py-4">
+        MySelf Nadim Chowdhury
       </h2>
-      <h3 className="text-xl sm:text-3xl">I&apos;m a Web Developer</h3>
+      <h3 className="text-2xl sm:text-3xl">A Web Developer</h3>
       <h3 className="text-[12px] sm:text-lg mt-4">
         <Typewriter
           options={{

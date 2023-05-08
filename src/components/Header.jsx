fdb7d4866aaf4e28674b67d4b4d574fbd492e879
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-05-08 14:21:15 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,21 +31,21 @@ export default function Header() {
         <Link
           href="/"
           onClick={() => setShow(!show)}
-          className="px-2 py-1 hover:bg-slate-100 transition duration-300"
+          className="p-2 hover:bg-slate-100 transition duration-300"
         >
           Home
         </Link>
         <Link
           href="/projects"
           onClick={() => setShow(!show)}
-          className="px-2 py-1 hover:bg-slate-100 transition duration-300"
+          className="p-2 hover:bg-slate-100 transition duration-300"
         >
           Projects
         </Link>
         <Link
           href="/certificates"
           onClick={() => setShow(!show)}
-          className="px-2 py-1 hover:bg-slate-100 transition duration-300"
+          className="p-2 hover:bg-slate-100 transition duration-300"
         >
           Certificates
         </Link>

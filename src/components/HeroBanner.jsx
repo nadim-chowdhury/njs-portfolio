224772f816058b4413acdc7e08c1e5af02fbb4b2
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2023-05-08 14:40:03 +0600

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -9,7 +9,7 @@ export default function HeroBanner() {
         MySelf Nadim Chowdhury
       </h2>
       <h3 className="text-2xl sm:text-4xl">As a Web Developer</h3>
-      <h3 className="text-[12px] sm:text-lg mt-4 pl-[3px] py-2">
+      <h3 className="text-[12px] sm:text-lg mt-4 pl-[4px] py-2">
         <Typewriter
           options={{
             strings: [

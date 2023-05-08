06d743aeb3aba8c2e6dda5ae459ec4a72d0cac61
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-05-08 13:09:18 +0600

    font weight changed

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,11 +4,11 @@ import Typewriter from "typewriter-effect";
 export default function HeroBanner() {
   return (
     <div className="py-9 sm:mx-3 lg:mx-0 text-slate-700">
-      <h3 className="text-4xl sm:text-5xl">Hello World!</h3>
-      <h2 className="text-3xl font-bold sm:text-5xl py-6">
+      <h3 className="text-3xl sm:text-5xl">Hello World!</h3>
+      <h2 className="text-2xl font-bold sm:text-5xl py-6">
         MySelf Nadim Chowdhury
       </h2>
-      <h3 className="text-3xl sm:text-4xl">As a Web Developer</h3>
+      <h3 className="text-2xl sm:text-4xl">As a Web Developer</h3>
       <h3 className="text-[12px] sm:text-lg mt-4 pl-[2px]">
         <Typewriter
           options={{
